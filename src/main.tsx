79a3770ba29b55618396ba4d983bfe49710b8b6d
denysveklych <denys.veklych@guesty.com>
2025-12-07 16:11:30 +0100

    main: vite base

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import HomePage from './pages/Home.page.tsx'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import './index.css'
@@ -14,29 +14,15 @@ const theme = createTheme({
   },
 })
 
-// eslint-disable-next-line react-refresh/only-export-components
-const RoutesWrapper = () => {
-  const location = useLocation()
-  console.log(location, 'location')
-  return (
-    <Routes location={location}>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  )
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-const App = () => {
-  return (
-    <StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <RoutesWrapper />
-        </BrowserRouter>
-      </ThemeProvider>
-    </StrictMode>
-  )
-}
-
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
+)
