3ab73470e3d886c204ff21a252cf3278f52a585d
denysveklych <denys.veklych@guesty.com>
2026-01-06 14:15:39 +0100

    results-bug: base_url change

--- e2e/test-credentials.ts
+++ e2e/test-credentials.ts
@@ -3,5 +3,4 @@ export const TEST_USER = {
   password: process.env.TEST_USER_PASSWORD || 'qweqwe123123',
 }
 
-export const BASE_URL = process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:5173'
-
+export const BASE_URL = 'http://localhost:5173'
